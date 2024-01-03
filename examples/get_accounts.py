f8fa05fa9bc4c8090664393f6b3727fef48ee983
Peter McDonald <148006+petermcd@users.noreply.github.com>
2024-01-03 02:25:55 +0000

    Updated example

--- examples/get_accounts.py
+++ examples/get_accounts.py
@@ -6,9 +6,9 @@ from monzo.exceptions import MonzoError
 client_id = ''  # Client ID obtained when creating Monzo client
 client_secret = ''  # Client secret obtained when creating Monzo client
 redirect_uri = 'http://127.0.0.1/monzo'  # URL requests via Monzo will be redirected in a browser
-access_token = ''
-expiry = 0
-refresh_token = ''
+access_token = ''  # access_token retrieved in step 2
+expiry = 0  # access_token_expiry retrieved in step 2
+refresh_token = ''  # refresh_token retrieved in step 2\
 
 monzo = Authentication(
     client_id=client_id,
@@ -22,7 +22,9 @@ monzo = Authentication(
 try:
     accounts = Account.fetch(monzo)
     for account in accounts:
-        _ = account.balance
-    print(len(accounts))
+        print(
+            f'Account ID: {account.account_type()} - '
+            f'Balance: {account.balance.total_balance if account.balance else 0}'
+        )
 except MonzoError:
     print('Failed to retrieve accounts')
